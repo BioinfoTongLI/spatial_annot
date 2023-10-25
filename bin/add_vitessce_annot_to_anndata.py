e4139de46e02a922dabc7c3bc1bbf587696255b4
Tong LI <tl10@sanger.ac.uk>
2023-10-25 22:33:54 +0000

    try to load local zarr. If fail, try s3 loader

--- bin/add_vitessce_annot_to_anndata.py
+++ bin/add_vitessce_annot_to_anndata.py
@@ -7,6 +7,24 @@
 import fire
 import json
 import anndata as ad
+import s3fs
+
+
+def read_remote_zarr(s3_url: str) -> ad.AnnData:
+    # pip install s3fs zarr anndata
+
+    # horribleness for Sanger
+    s3 = s3fs.S3FileSystem(anon=True, client_kwargs = {
+        'endpoint_url':"https://cog.sanger.ac.uk",
+        'region_name':'us-east-1'
+    })
+
+    # path to anndata.zarr
+    # s3_store = 'xenium/public/KR_XEN/KR_XEN-output-XETG00155__0004120__Region_2__20231011__125831-anndata.zarr'
+
+    # create zarr store
+    store = s3fs.S3Map(root=s3_url,s3=s3)
+    return ad.read_zarr(store)
 
 
 def main(annot_json:str, anndata_zarr:str, out:str):
@@ -15,7 +33,10 @@ def main(annot_json:str, anndata_zarr:str, out:str):
     for child in config["tree"][0]['children']:
         selected_ids = [i[0] for i in child['set']]
 
-    adata = ad.read_zarr(anndata_zarr)
+    try:
+        adata = ad.read_zarr(anndata_zarr)
+    except:
+        adata = read_remote_zarr(anndata_zarr)
     adata.obs["Annotation"] = "NA"
     adata.obs.loc[selected_ids, "Annotation"] = config["tree"][0]['children'][0]['name']
     zarr_out = f"{out}_anndata_with_annotations.h5ad"
